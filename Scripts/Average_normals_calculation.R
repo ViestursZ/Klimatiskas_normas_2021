a1a49079be942c4d20e76c0ced805a8bd1355517
Viesturs <viesturs.zandersons@gmail.com>
2021-03-29 14:44:52 +0300

    Renamed the Vid_normas_rezultati script to Average_normals_calculation.R script.

--- Scripts/Average_normals_calculation.R
+++ Scripts/Average_normals_calculation.R
@@ -1,34 +1,15 @@
 
-# No DB līdz gala tibble
-
-#### Pieprasījums no DB ####
-con <- source("P:/KMN/Kodi/Clidata_connection.r")
-query <- "select *
-from CLIDATA.RDATA RDATA
-where EG_EL_ABBREVIATION in ('TDRY', 'HTDRY')
-and EG_GH_ID in ('RIAI99PA', 'RIAL99MS', 'RIBA99PA', 'RIDM99MS', 'RIDO99MS', 'RIGU99MS', 
-'RIJE99PA', 'RIKO99PA', 'RILP99PA', 
-'RIME99MS', 'RIPA99PA', 'RIPR99PA', 'RIRU99PA', 'RIRE99MS',
-'RISA99PA', 'RISE99MS', 'RISI99PA', 'RIST99PA', 'RIVE99PA', 
-'RIZI99PA','RIZO99MS')
-and YEAR > 1989
-;"
-
-data <- sqlQuery(con[[1]], query,
-                 stringsAsFactors = F, dec = ",")[,c(1:37)]
-
-data_sakums <- data_prep(data) # Sagatavo datus
+# Funkcijas --------------------------------------------------------------------
 
 # turpmākajām f-jām nepieciešams, lai dati satur kolonnas - EG_GH_ID, Value un DATE 
-# DATE formāts as.POSIXct %Y %m %d%H:%M
-data_sakums <- data_sakums[,-2]
+source("D:/Viesturs_Zandersons/Klimatiskas_normas/Scripts/Norma_vid_fjas.R", encoding = "UTF-8")
 
 vid_normas_apr <- function(x, time){  # time jāliek diena = 1, dekāde = 2, menesis = 3, gads = 4, nedēļa = 5
-  if(time>5){
+  if(time > 5){
     "Aplami ievadits time. Dienai time = 1, dekadei time = 2, menesim time = 3, gadam time = 4, nedēļai time = 5."
   } else if(time == 1){
-    diennakts <- aver_value(x, time = 1)
-    ilggad_dien <- ilggad_value(diennakts, time = 1, type = 1)
+    # diennakts <- aver_value(x, time = 1) # Diennakts dati jau ir aprēķināti
+    ilggad_dien <- ilggad_value(x, time = 1, type = 1)
     dien_norm <- day_norma(ilggad_dien)
     dien_norm_tib <- norm_tibble(dien_norm)
   } else if(time == 2){
@@ -44,17 +25,32 @@ vid_normas_apr <- function(x, time){  # time jāliek diena = 1, dekāde = 2, men
     ilggad_year <- ilggad_value(gads, time = 4, type = 1)
     ilggad_year_tib <- norm_tibble(ilggad_year)
   } else {
-    diennakts <- aver_value(x, time = 1)
+    # diennakts <- aver_value(x, time = 1)
+    diennakts <- x
     ilggad_dien <- ilggad_value(diennakts, time = 1, type = 1)
     ned_norm <- ned_norma(ilggad_dien)
     ned_norm_tib <- norm_tibble(ned_norm)
   }
 }
 
-day_normal <- vid_normas_apr(data_sakums, time = 1)
-dec_normal <- vid_normas_apr(data_sakums, time = 2)
-month_normal <- vid_normas_apr(data_sakums, time = 3)
-year_normal <- vid_normas_apr(data_sakums, time = 4)
+
+# Ielādē datus -------------------------------------------------------------
+
+temp_daily <- read_csv("Dati/MeanT_daily.csv")
+
+# Parastās normas
+temp_daily <- temp_daily %>%
+  mutate(Merijums = ifelse(Merijums == -999.9, NA, Merijums))
+
+# Pārsauc kolonnas, lai normāli rēķinātos normas
+temp_daily <- temp_daily %>%
+  set_colnames(c("DATE", "EG_GH_ID", "Value"))
+
+
+uh_day_normal <- vid_normas_apr(temp_daily, time = 1)
+uh_dec_normal <- vid_normas_apr(temp_daily, time = 2)
+uh_month_normal <- vid_normas_apr(temp_daily, time = 3)
+uh_year_normal <- vid_normas_apr(temp_daily, time = 4)
 
 
 
