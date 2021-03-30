286b3f1789571d5018c401372dedd39850c9a2dd
Viesturs <viesturs.zandersons@gmail.com>
2021-03-30 10:45:25 +0300

    MeanT homogenizācijā apvienotas Dagdas un Rēzeknes stacijas.

--- Scripts/Temperaturas_homogenizacija.R
+++ Scripts/Temperaturas_homogenizacija.R
@@ -14,8 +14,12 @@ source("D:/Viesturs_Zandersons/Scripts/Noderigas_R_funkcijas/Recode_stations.R",
 
 # Ielādē sakārtotos temperatūras datus ------------------------------------
 
-## NEPIESKAITĪJU ZIEMAS LAIKU
+temp_dati <- read_csv("Dati/Temp_dati_clean.csv")
 
+temp_dati <- temp_dati %>%
+  mutate(Datums_laiks = ymd_hms(Datums_laiks))
+
+## NEPIESKAITĪJU ZIEMAS LAIKU
 temp_d <- temp_dati %>%
   mutate(Datums = date(Datums_laiks)) %>%
   group_by(Stacija, Datums) %>%
@@ -28,11 +32,17 @@ temp_d <- temp_d %>%
 # Kādas ir stacijas
 data_stacs <- temp_d %$% unique(Stacija)
 
-# Jāapvieno Rīgas, jāatfiltrē lidosta
+# Jāapvieno Rīgas
 temp_d <- temp_d %>%
   spread(Stacija, Merijums) %>%
   mutate(RIGASLU = ifelse(is.na(RIGASLU), RIAS99PA, RIGASLU)) %>%
   select(-RIAS99PA)
+
+# Jāapvieno Dagdas un Rēzeknes
+temp_d <- temp_d %>%
+  mutate(RIREZEKN = ifelse(is.na(RIREZEKN), RIRE99MS, RIREZEKN)) %>%
+  mutate(RIDAGDA = ifelse(is.na(RIDAGDA), DAGDA, RIDAGDA)) %>%
+  select(-DAGDA, -RIRE99MS)
 
 # Pievieno iztrūkstosos datumus
 iztr_dateseq <- seq(min(temp_d$Datums), max(temp_d$Datums),
@@ -43,7 +53,7 @@ temp_d <- temp_d %>% left_join(iztr_df, .,) %>%
   gather(Stacija, Merijums, -Datums) %>%
   mutate(Merijums = replace_na(Merijums, -999.9)) # ACMANT NA
 
-data_stacs <- data_stacs[data_stacs != "RIAS99PA"]
+data_stacs <- data_stacs[!data_stacs %in% c("RIAS99PA", "RIRE99MS", "RIDAGDA")]
 
 temp_d %>%
   write_csv("Dati/MeanT_daily.csv")
