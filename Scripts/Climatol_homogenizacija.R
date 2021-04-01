8c63bce897927a5c2b677f3d5859e5d19881763f
Viesturs <viesturs.zandersons@gmail.com>
2021-04-01 10:35:18 +0300

    Setupota CLIMATOL homogenizācija

--- Scripts/Climatol_homogenizacija.R
+++ Scripts/Climatol_homogenizacija.R
@@ -7,6 +7,11 @@ library(lubridate)
 library(magrittr)
 library(sf)
 
+# Functions ---------------------------------------------------------------
+
+source("D:/Viesturs_Zandersons/Scripts/Noderigas_R_funkcijas/Recode_stations.R",
+       encoding = "UTF-8")
+
 # Sakārto datus -----------------------------------------------------------
 
 temp_daily <- read_csv("Dati/MeanT_daily.csv")
@@ -44,6 +49,10 @@ climatol_temp_coords <- climatol_temp_coords %>%
 climatol_temp_coords <- climatol_temp_coords %>%
   arrange(GH_ID)
 
+climatol_temp_coords <- climatol_temp_coords %>% 
+  mutate(Stacija = recode_stations(GH_ID, "ID-LV")) %>%
+  mutate(Stacija = ifelse(Stacija == "Rīga Universitāte", "Rīga", Stacija)) %>%
+  mutate(Stacija = ifelse(Stacija == "RIGAAIR", "Rīga-Lidosta", Stacija))
 
 # Nomet liekās stacijas ---------------------------------------------------
 
@@ -53,24 +62,52 @@ temp_daily %>%
   mutate(Gads = year(Datums)) %>%
   group_by(Stacija, Gads) %>%
   summarise(NA_skaits = sum(is.na(Merijums))) %>%
-  spread(Stacija, NA_skaits) %>%
-  View()
+  spread(Stacija, NA_skaits)
 
-# Tādi normāli mērījumi sākas no 1927. gada. 
+# Tādi normāli priekš CLIMATOL mērījumi sākas no 1947. gada. 
 climatol_temp_daily <- temp_daily %>%
-  filter(year(Datums) >= 1927)
+  filter(year(Datums) >= 1947)
 
-# Nost metamās stacijas
-navajag_clim_stacs <- climatol_temp_daily %>%
+climatol_temp_daily <- climatol_temp_daily %>%
+  arrange(Stacija, Datums) %>%
+  filter(year(Datums) <= 2020)
+
+# Nost metamās stacijas - CLIMATOL nestrādā
+nevajag_clim_stacs <- climatol_temp_daily %>%
   group_by(Stacija) %>%
   summarise(Mer_skaits = n(),
             NA_skaits = sum(is.na(Merijums))) %>% 
   filter(NA_skaits >= 11444) %>%
   pull(Stacija)
 
+climatol_temp_daily <- climatol_temp_daily %>%
+  filter(!Stacija %in% nevajag_clim_stacs)
+
+climatol_temp_coords <- climatol_temp_coords %>%
+  filter(!GH_ID %in% nevajag_clim_stacs)
+
 # Pagaidām gan stacijas nost nemet
+# Sakārto un ieraksta datus atbilstoši CLIMATOL formātam -----------------------
+write(climatol_temp_daily$Merijums, "Dati/Climatol_data/LVMeanT_1947-2020.dat", 
+      ncolumns = 5) # Ieraksta Climatol formātā
 
-# Sakārto atbilstoši CLIMATOL formātam ------------------------------------
+climatol_temp_coords %>%
+  write.table("Dati/Climatol_data/LVMeanT_1947-2020.est",
+              row.names = F, col.names = F) # Ieraksta coords Climatol formātā
 
+
+# Climatol homogenizācija -------------------------------------------------
+setwd("./Dati/Climatol_data")
+
+# No sākuma viens test run
+# homogen("LVMeanT", 1947, 2020, expl = T)
+# outrename("LVMeanT", 1947, 2020, "expl_analysis")
+
+homogen("LVMeanT", 1947, 2020)
+outrename("LVMeanT", 1947, 2020, "test")
+setwd("../../")
+
+
+# Test run over -----------------------------------------------------------
 
 
