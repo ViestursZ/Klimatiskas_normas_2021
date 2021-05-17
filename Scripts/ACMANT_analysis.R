7779ea2e847e6f23caf34d34c215554b08d6ca6d
Viesturs <viesturs.zandersons@gmail.com>
2021-05-17 10:15:42 +0300

    ACMANT_analysis aizkomentētas nekoriģētās datu kopas

--- Scripts/ACMANT_analysis.R
+++ Scripts/ACMANT_analysis.R
@@ -8,14 +8,14 @@ library(magrittr)
 # Ielādē neapstrādātos temperatūras ACMANT homogenizētus datus ------------
 
 # Listo failus
-ACMANT_files <- list.files("Dati/ACMANT_homogenized_data/MeanT/Raw/", pattern = "v.txt$", full.names = T)
-ACMANT_reliabilityf <- list.files("Dati/ACMANT_homogenized_data/MeanT/Raw/", pattern = "i.txt$")
-ACMANT_breaksf <- "Dati/ACMANT_homogenized_data/MeanT/Raw/AvgTn_breaks.txt"
-
-ACMANT_data <- purrr::map(ACMANT_files, read_table, skip = 1, col_names = F, skip_empty_rows = F)
-ACMANT_stations <- purrr::map_chr(ACMANT_files, read_lines, n_max = 1)
-
-ACMANT_stations <- str_remove_all(ACMANT_stations, " ")
+# ACMANT_files <- list.files("Dati/ACMANT_homogenized_data/MeanT/Raw/", pattern = "v.txt$", full.names = T)
+# ACMANT_reliabilityf <- list.files("Dati/ACMANT_homogenized_data/MeanT/Raw/", pattern = "i.txt$")
+# ACMANT_breaksf <- "Dati/ACMANT_homogenized_data/MeanT/Raw/AvgTn_breaks.txt"
+# 
+# ACMANT_data <- purrr::map(ACMANT_files, read_table, skip = 1, col_names = F, skip_empty_rows = F)
+# ACMANT_stations <- purrr::map_chr(ACMANT_files, read_lines, n_max = 1)
+# 
+# ACMANT_stations <- str_remove_all(ACMANT_stations, " ")
 
 
 # Ielādē apstrādātos temperatūras ACMANT homogenizētus datus --------------
@@ -31,16 +31,16 @@ ACMANT_kor_stations <- str_remove_all(ACMANT_kor_stations, " ")
 
 # Datu sakārtošana --------------------------------------------------------
 
-ACMANT_data_t <- ACMANT_data %>%
-  purrr::map2(ACMANT_stations, ~mutate(.x, Stacija = .y)) %>%
-  purrr::map(~select(.x, Stacija, everything())) %>%
-  bind_rows() %>%
-  set_colnames(c("Stacija", "Gads", "Menesis", seq(1:31))) %>%
-  pivot_longer(cols = paste(seq(1:31)), names_to = "Diena", values_to = "Merijums") %>%
-  mutate(Merijums = ifelse(Merijums == -999.9, NA, Merijums),
-         Datums = ymd(str_c(Gads, Menesis, Diena, sep = "-"))) %>%
-  filter(!is.na(Datums)) %>%
-  select(-Gads, -Menesis, -Diena)
+# ACMANT_data_t <- ACMANT_data %>%
+#   purrr::map2(ACMANT_stations, ~mutate(.x, Stacija = .y)) %>%
+#   purrr::map(~select(.x, Stacija, everything())) %>%
+#   bind_rows() %>%
+#   set_colnames(c("Stacija", "Gads", "Menesis", seq(1:31))) %>%
+#   pivot_longer(cols = paste(seq(1:31)), names_to = "Diena", values_to = "Merijums") %>%
+#   mutate(Merijums = ifelse(Merijums == -999.9, NA, Merijums),
+#          Datums = ymd(str_c(Gads, Menesis, Diena, sep = "-"))) %>%
+#   filter(!is.na(Datums)) %>%
+#   select(-Gads, -Menesis, -Diena)
 
 ACMANT_kor_data_t <- ACMANT_kor_data %>%
   purrr::map2(ACMANT_kor_stations, ~mutate(.x, Stacija = .y)) %>%
